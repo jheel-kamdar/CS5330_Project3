2a61fd63698788fdd41c6ffdea9b8932a9d63aab
Jheel Kamdar <144502229+jheel-kamdar@users.noreply.github.com>
2025-02-20 20:50:02 -0500

    Add files via upload

---
+++ build/CMakeFiles/ObjectRecognition.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ObjectRecognition.dir/src/main.cpp.o"
+  "CMakeFiles/ObjectRecognition.dir/src/main.cpp.o.d"
+  "ObjectRecognition"
+  "ObjectRecognition.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ObjectRecognition.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
